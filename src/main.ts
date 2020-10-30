80165853c1555c1ec027946d26323193d75b6ff0
Josh Pinkney <joshpinkney@gmail.com>
2020-10-30 13:49:17 -0400

    Initial nightly updater nightly
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as core from '@actions/core'
-import { getLastestMetaYAMLVersion } from './create';
-import { updateLatestTxt, updateVSCodeExtensionsJSON } from './update';
-import { createNewMetaYAML, writeNewMetaYAML } from './modify';
 import * as github from '@actions/github';
+import * as fs from 'fs';
 
 /**
  * Main function that is ran when the github action is started
@@ -10,18 +8,25 @@ import * as github from '@actions/github';
 async function run(): Promise<void> {
   try {
 
-    const myToken = core.getInput('myToken');
+    const vscodeExtensionsPath = "vscode-extensions.json";
+    const userToken = core.getInput('token');
 
-    const octokit = github.getOctokit(myToken)
-    
-    const extensionsJSON = require('./vscode-extensions.json')
-    for (const ext of extensionsJSON.extensions) {
+    const octokit = github.getOctokit(userToken)
+
+    const extensionsFile = fs.readFileSync(vscodeExtensionsPath, 'utf-8');
+    const extensionsJSON = JSON.parse(extensionsFile);
+    console.log(extensionsJSON);
+    for (const extIndex in extensionsJSON.extensions) {
       // Check the repository to see if it has a newer tag
+      const ext = extensionsJSON.extensions[extIndex];
       const repository = ext.repository;
-      const tags = octokit.repos.listTags({
-        owner: "a",
-        repo: repository
+      const websiteOwnerRepo = getWebsiteOwnerRepo(repository);
+      console.log(websiteOwnerRepo);
+      const tags = await octokit.repos.listTags({
+        owner: websiteOwnerRepo.owner,
+        repo: websiteOwnerRepo.repo
       });
+      console.log(tags);
       /**
        * [
           {
@@ -35,23 +40,47 @@ async function run(): Promise<void> {
           }
         ]
        */
-      // test to see if the revision matches name
-      // if it fails then test to see if the revision matches commit.sha
+
+      /**
+       * Get the first tag in the response. It's guarenteed to be the newest.
+       * If the name and the sha are not equal to the revision number in vscode-extensions.json
+       * then we need to update because there is a newer version
+       */
+      const firstTag = tags.data.length > 0 ? tags.data[0] : undefined;
+      if (firstTag) {
+        if (firstTag.name != ext.revision && firstTag.commit.sha != ext.revision) {
+          // We need to update
+          extensionsJSON.extensions[extIndex].revision = firstTag.name;
+          console.log("here");
+          console.log(firstTag.name);
+          console.log(extensionsJSON.extensions[extIndex].revision)
+        }
+      }
+      // Technically we need to handle the case when revision is a sha1 nightly but I'll ignore that for now
     }
 
-
-
-    const latest = getLastestMetaYAMLVersion()
-    const newMetadata = createNewMetaYAML(latest, next)
-    writeNewMetaYAML(newMetadata)
-    updateLatestTxt(next)
-    updateVSCodeExtensionsJSON(newMetadata)
-
-    core.setOutput('name', newMetadata.name)
-    core.setOutput('version', next)
+    fs.writeFileSync(vscodeExtensionsPath, 'utf-8');
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+interface WebsiteOwnerRepo {
+  website: string;
+  owner: string;
+  repo: string;
+}
+
+function getWebsiteOwnerRepo(repository: string): WebsiteOwnerRepo {
+  // repository https://github.com/asciidoctor/asciidoctor-vscode
+  const uri = new URL(repository);
+  const ownerRepo = uri.pathname.split("/"); // Gets something like [ "", "asciidoctor", "asciidoctor-vscode" ]
+  return {
+    website: uri.origin,
+    owner: ownerRepo[1],
+    repo: ownerRepo[2]
+  }
+
 }
 
 run()
